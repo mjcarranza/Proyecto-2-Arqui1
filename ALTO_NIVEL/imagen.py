ada56a0cc67808e6a1e6a993634b6cba1257675a
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2024-05-11 20:32:58 -0600

    Cambios

--- ALTO_NIVEL/imagen.py
+++ ALTO_NIVEL/imagen.py
@@ -1,10 +1,27 @@
 from PIL import Image
 
-imagen = Image.open("./ALTO_NIVEL/logo.png")
-datos = list(imagen.getdata())
-ancho,alto = imagen.size
-cab = f"24 {ancho} {alto}\n"
-contenido_mif = cab + "\n".join(f"{r} {g} {b}" for r, g, b, _ in datos)
+imagen = Image.open("./ALTO_NIVEL/bl.jpeg")
+datos_pixeles = imagen.convert("RGB").tobytes()
 
-with open("./ALTO_NIVEL/imagen.mif", "w") as archivo:
-    archivo.write(contenido_mif)
+
+
+with open("./ALTO_NIVEL/imagen.mif", "w") as archivo_mif:
+    archivo_mif.write("WIDTH=24;\n")
+    archivo_mif.write("DEPTH={};\n".format(imagen.width * imagen.height))
+    archivo_mif.write("ADDRESS_RADIX=DEC;\n")  # Cambiamos a decimal
+    archivo_mif.write("DATA_RADIX=HEX;\n")
+    archivo_mif.write("CONTENT BEGIN\n")
+
+    for y in range(imagen.height):
+        for x in range(imagen.width):
+            pixel = imagen.getpixel((x, y))
+            valor_hex = '{0:02X}{1:02X}{2:02X}'.format(*pixel)
+            direccion_decimal = y * imagen.width + x  # Calculamos la dirección en decimal
+            archivo_mif.write("   {:d} : {:s};\n".format(direccion_decimal, valor_hex))  # Escribimos la dirección en decimal
+
+    archivo_mif.write("END;")
+
+
+
+# Ejemplo de uso
+#image_to_mif("./ALTO_NIVEL/negro.jpeg", "./ALTO_NIVEL/imagen.mif")
